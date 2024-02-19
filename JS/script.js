9bb1da9159453cb558cf87dde90ee9e83c14a649
Rakib-Akanda <rmd04037@gmail.com>
2024-02-19 14:21:14 +0600

    Added scroll function to banner button

---
+++ JS/script.js
@@ -0,0 +1,7 @@
+function bannerBtnToPHpribahan() {
+  const targetSection = document.getElementById("target-section");
+  const selectButton = document.getElementById("btn-buy-ticket");
+  selectButton.addEventListener("click", bannerBtnToPHpribahan);
+  targetSection.scrollIntoView({ behavior: "smooth" });
+}
+
